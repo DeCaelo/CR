f343f455b4ebe7244ceea8d8da35de0b26dbc3d4
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-11-06 11:50:08 +0100

    establish the socket connection in server-side (routes.rb) & client-side (chatrooms.js)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "welcome#about"
+
   resources :chatrooms, param: :slug
   resources :messages
+
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
 end
